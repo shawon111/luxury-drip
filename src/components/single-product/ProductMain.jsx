d8caaccae739a51d4420ac1f8f676029dd4123e3
shawon111 <shawonahmmed08509@gmail.com>
2025-06-09 23:35:52 +0600

    different optiuons for shoes

--- src/components/single-product/ProductMain.jsx
+++ src/components/single-product/ProductMain.jsx
@@ -9,6 +9,10 @@ import Image from 'next/image';
 const ProductMain = ({ product, translations }) => {
     const { title, originalPrice, discountedPrice, featuredImages } = product;
     const isBag = title.toLowerCase().includes('bag');
+    const isShoe = ['shoe', 'sneaker', 'skate', 'jordan', 'j4', 'trainer',
+  'cinturino', 'yzy', 'yeezy', 'campus', 'boost'].some(keyword =>
+        title.toLowerCase().includes(keyword)
+    );
     return (
         <section className='pb-[80px]'>
             <div className="max-w-full lg:max-w-[1170px] mx-auto px-[10px] lg:px-0">
@@ -37,15 +41,37 @@ const ProductMain = ({ product, translations }) => {
                                         <SelectContent>
                                             <SelectItem value="unique">Unique</SelectItem>
                                         </SelectContent>
-                                    ) : (
+                                    ) : isShoe ? (
                                         <SelectContent>
-                                            <SelectItem value="l">L</SelectItem>
-                                            <SelectItem value="m">M</SelectItem>
-                                            <SelectItem value="s">S</SelectItem>
-                                            <SelectItem value="xl">XL</SelectItem>
-                                            <SelectItem value="xs">XS</SelectItem>
+                                            <SelectItem value="36">36</SelectItem>
+                                            <SelectItem value="36.5">36.5</SelectItem>
+                                            <SelectItem value="37">37</SelectItem>
+                                            <SelectItem value="37.5">37.5</SelectItem>
+                                            <SelectItem value="38">38</SelectItem>
+                                            <SelectItem value="38.5">38.5</SelectItem>
+                                            <SelectItem value="39">39</SelectItem>
+                                            <SelectItem value="39.5">39.5</SelectItem>
+                                            <SelectItem value="40">40</SelectItem>
+                                            <SelectItem value="40.5">40.5</SelectItem>
+                                            <SelectItem value="41">41</SelectItem>
+                                            <SelectItem value="41.5">41.5</SelectItem>
+                                            <SelectItem value="42">42</SelectItem>
+                                            <SelectItem value="42.5">42.5</SelectItem>
+                                            <SelectItem value="43">43</SelectItem>
+                                            <SelectItem value="43.5">43.5</SelectItem>
+                                            <SelectItem value="44">44</SelectItem>
+                                            <SelectItem value="44.5">44.5</SelectItem>
+                                            <SelectItem value="45">45</SelectItem>
+                                            <SelectItem value="45.5">45.5</SelectItem>
+                                            <SelectItem value="46">46</SelectItem>
                                         </SelectContent>
-                                    )
+                                    ) : <SelectContent>
+                                        <SelectItem value="l">L</SelectItem>
+                                        <SelectItem value="m">M</SelectItem>
+                                        <SelectItem value="s">S</SelectItem>
+                                        <SelectItem value="xl">XL</SelectItem>
+                                        <SelectItem value="xs">XS</SelectItem>
+                                    </SelectContent>
                                 }
                             </Select>
                             <Button className="text-sm font-[400] text-white uppercase mt-6 rounded-none min-h-[50px]"><a target='_blank' href="https://wa.me/393508301446">{translations.whats_app_btn}</a></Button>
